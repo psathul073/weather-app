f91a1c201fbacb8589b96936b0a19f3a1b6628c3
Athul PS <psathul073@gmail.com>
2025-09-29 12:02:55 +0530

    fix searchbar issue

--- src/components/Search-box.jsx
+++ src/components/Search-box.jsx
@@ -1,60 +1,145 @@
-import { useEffect, useRef, useState } from "react"
-import Icons from "../icons/Icons"
+import { useEffect, useRef, useState } from "react";
+import Icons from "../icons/Icons";
 import Settings from "./Settings";
 
-const SearchBox = ({ city, setCity, unit, setUnit, fetchGeoWeather, fetchWeather, loading }) => {
+const SearchBox = ({
+  city,
+  setCity,
+  unit,
+  setUnit,
+  fetchGeoWeather,
+  fetchWeather,
+  loading,
+}) => {
+  const [searchBox, setSearchBox] = useState(false);
+  const [settingsMenu, setSettingsMenu] = useState(false);
+  const searchTriggerRef = useRef();
+  const searchDropdownRef = useRef();
 
-    const [searchBox, setSearchBox] = useState(false);
-    const [settingsMenu, setSettingsMenu] = useState(false);
-    const searchTriggerRef = useRef();
-    const searchDropdownRef = useRef();
+  // Close search box on outside click.
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      // console.log(e.target);
+      const clickedOutsideButtons =
+        searchTriggerRef.current &&
+        !searchTriggerRef.current.contains(e.target);
+      const clickedOutsideDropdown =
+        searchDropdownRef.current &&
+        !searchDropdownRef.current.contains(e.target);
 
-    // Close search box on outside click.
-    useEffect(() => {
-        const handleClickOutside = (e) => {
-            // console.log(e.target);
-            const clickedOutsideButtons = searchTriggerRef.current && !searchTriggerRef.current.contains(e.target);
-            const clickedOutsideDropdown = searchDropdownRef.current && !searchDropdownRef.current.contains(e.target);
+      if (clickedOutsideButtons && clickedOutsideDropdown) {
+        setSearchBox(false);
+      }
+    };
+    document.addEventListener("pointerdown", handleClickOutside);
+    return () =>
+      document.removeEventListener("pointerdown", handleClickOutside);
+  }, []);
 
-            if (clickedOutsideButtons && clickedOutsideDropdown) {
-                setSearchBox(false);
-            }
-        };
-        document.addEventListener('pointerdown', handleClickOutside);
-        return () => document.removeEventListener('pointerdown', handleClickOutside);
+  return (
+    <>
+      {/* Search box container */}
+      <div className="search-box-1 w-full hidden sm:flex flex-row items-center px-1.5 py-1 rounded-xl text-xl dark:text-white bg-zinc-100 dark:bg-zinc-900">
+        <Icons name={"globeSearch"} />
 
-    }, []);
+        <input
+          type="text"
+          className=" w-full p-1.5 outline-0 text-base"
+          placeholder="Search for places..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
 
-    return (
         <>
-            {/* Search box container */}
-            <div className="search-box-1 w-full hidden sm:flex flex-row items-center text-xl dark:text-white">
-                <button disabled={loading} onClick={() => fetchWeather(city)} className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 cursor-pointer "><Icons name={'search'} /></button>
-                <input type="text" className=" w-full p-1.5 outline-0 text-base" placeholder="Search for places..." value={city} onChange={(e) => setCity(e.target.value)} />
-                <button disabled={loading} onClick={() => fetchGeoWeather()} className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover"><Icons name={'location'} /></button>
-            </div>
-
-            {/* For mobile devices */}
-            <div ref={searchTriggerRef} className="search-section relative w-full sm:hidden text-2xl flex flex-row items-center justify-between dark:text-white z-10 ">
-                <button className=" bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover" onClick={() => setSearchBox(!searchBox)}><Icons name={'add'} /></button>
-
-                <button className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover" onClick={() => { setSettingsMenu(true); setSearchBox(false) }}><Icons name={'settings'} /></button>
-
-                {searchBox &&
-                    <div ref={searchDropdownRef} className="search-box-2 absolute top-11 px-2 py-2 right-0 w-full sm:hidden flex flex-row items-center rounded-sm shadow-2xl bg-stone-100 dark:bg-stone-900 ">
-                        <button disabled={loading} onClick={() => fetchWeather(city)} className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 cursor-pointer "><Icons name={'search'} /></button>
-                        <input type="text" className=" w-full p-1.5 outline-0 text-base" placeholder="Search for places..." value={city} onChange={(e) => setCity(e.target.value)} />
-                        <button disabled={loading} onClick={() => fetchGeoWeather()} className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover"><Icons name={'location'} /></button>
-                   
-                    </div>
-                }
-            </div>
-
-            {
-                settingsMenu && <Settings setSettingsMenu={setSettingsMenu} unit={unit} setUnit={setUnit} />
-            }
+          {city.length > 0 ? (
+            <button
+              disabled={loading}
+              onClick={() => fetchWeather(city)}
+              className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 cursor-pointer "
+            >
+              <Icons name={"search"} />
+            </button>
+          ) : (
+            <button
+              disabled={loading}
+              onClick={() => fetchGeoWeather()}
+              className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover"
+            >
+              <Icons name={"location"} />
+            </button>
+          )}
         </>
-    )
-}
+      </div>
 
-export default SearchBox
+      {/* For mobile devices */}
+      <div
+        ref={searchTriggerRef}
+        className="search-section relative w-full sm:hidden text-2xl flex flex-row items-center justify-between dark:text-white z-10 "
+      >
+        <button
+          className=" bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover"
+          onClick={() => setSearchBox(!searchBox)}
+        >
+          <Icons name={"add"} />
+        </button>
+
+        <button
+          className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover"
+          onClick={() => {
+            setSettingsMenu(true);
+            setSearchBox(false);
+          }}
+        >
+          <Icons name={"settings"} />
+        </button>
+
+        {searchBox && (
+          <div
+            ref={searchDropdownRef}
+            className="search-box-2 absolute top-11 px-2 py-2 right-0 w-full sm:hidden flex flex-row items-center rounded-xl shadow-2xl bg-zinc-100 dark:bg-zinc-900 animate-down "
+          >
+            <Icons name={"globeSearch"} />
+
+            <input
+              type="text"
+              className=" w-full p-1.5 outline-0 text-base"
+              placeholder="Search for places..."
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+
+            <>
+              {city.length > 0 ? (
+                <button
+                  disabled={loading}
+                  onClick={() => fetchWeather(city)}
+                  className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 cursor-pointer "
+                >
+                  <Icons name={"search"} />
+                </button>
+              ) : (
+                <button
+                  disabled={loading}
+                  onClick={() => fetchGeoWeather()}
+                  className="bg-zinc-100 dark:bg-zinc-900 rounded-full p-1.5 textHover"
+                >
+                  <Icons name={"location"} />
+                </button>
+              )}
+            </>
+          </div>
+        )}
+      </div>
+
+      {settingsMenu && (
+        <Settings
+          setSettingsMenu={setSettingsMenu}
+          unit={unit}
+          setUnit={setUnit}
+        />
+      )}
+    </>
+  );
+};
+
+export default SearchBox;
--- src/icons/Icons.jsx
+++ src/icons/Icons.jsx
@@ -2,45 +2,262 @@
 const Icons = ({ name, className }) => {
   const Icon = {
     search: (
-      <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.5 17.5L22 22m-2-11a9 9 0 1 0-18 0a9 9 0 0 0 18 0" color="currentColor" /></svg>
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M17.5 17.5L22 22m-2-11a9 9 0 1 0-18 0a9 9 0 0 0 18 0"
+          color="currentColor"
+        />
+      </svg>
+    ),
+    globeSearch: (
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        width="1.6em"
+        height="1.6em"
+      >
+        <path
+          fill="currentColor"
+          d="M2.049 9.112a8.001 8.001 0 1 1 9.718 8.692a1.5 1.5 0 0 0-.206-1.865l-.01-.01q.244-.355.47-.837a9.3 9.3 0 0 0 .56-1.592H9.744q.17-.478.229-1h2.82A15 15 0 0 0 13 10c0-.883-.073-1.725-.206-2.5H7.206l-.05.315a4.5 4.5 0 0 0-.971-.263l.008-.052H3.46q-.112.291-.198.595c-.462.265-.873.61-1.213 1.017m9.973-4.204C11.407 3.59 10.657 3 10 3s-1.407.59-2.022 1.908A9.3 9.3 0 0 0 7.42 6.5h5.162a9.3 9.3 0 0 0-.56-1.592M6.389 6.5c.176-.743.407-1.422.683-2.015c.186-.399.401-.773.642-1.103A7.02 7.02 0 0 0 3.936 6.5zm9.675 7H13.61a10.5 10.5 0 0 1-.683 2.015a6.6 6.6 0 0 1-.642 1.103a7.02 7.02 0 0 0 3.778-3.118m-2.257-1h2.733c.297-.776.46-1.62.46-2.5s-.163-1.724-.46-2.5h-2.733c.126.788.193 1.63.193 2.5s-.067 1.712-.193 2.5m2.257-6a7.02 7.02 0 0 0-3.778-3.118c.241.33.456.704.642 1.103c.276.593.507 1.272.683 2.015zm-7.76 7.596a3.5 3.5 0 1 0-.707.707l2.55 2.55a.5.5 0 0 0 .707-.707zM8 12a2.5 2.5 0 1 1-5 0a2.5 2.5 0 0 1 5 0"
+        />
+      </svg>
     ),
     location: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor"><path d="m20 11l2 1c0-5.523-4.477-10-10-10c-4.1 0-7.625 2.468-9.168 6M4 13l-2-1c0 5.523 4.477 10 10 10c4.1 0 7.625-2.468 9.168-6M12 11.5h.009" /><path d="M12 7c2.435 0 4.5 2.016 4.5 4.463c0 2.485-2.098 4.23-4.036 5.415a.94.94 0 0 1-.928 0C9.603 15.681 7.5 13.957 7.5 11.463C7.5 9.016 9.565 7 12 7" /></g></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          color="currentColor"
+        >
+          <path d="m20 11l2 1c0-5.523-4.477-10-10-10c-4.1 0-7.625 2.468-9.168 6M4 13l-2-1c0 5.523 4.477 10 10 10c4.1 0 7.625-2.468 9.168-6M12 11.5h.009" />
+          <path d="M12 7c2.435 0 4.5 2.016 4.5 4.463c0 2.485-2.098 4.23-4.036 5.415a.94.94 0 0 1-.928 0C9.603 15.681 7.5 13.957 7.5 11.463C7.5 9.016 9.565 7 12 7" />
+        </g>
+      </svg>
     ),
     C: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="2 0 24 24" width='1em' height='1em'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor"><circle cx="5" cy="6" r="2" /><path d="M21 7c-.044-.653-.144-1.113-.368-1.5a3 3 0 0 0-1.102-1.098C18.831 4 17.894 4 16.018 4s-2.814 0-3.513.402A3 3 0 0 0 11.403 5.5C11 6.196 11 7.13 11 9v6c0 1.87 0 2.804.403 3.5c.265.456.645.835 1.102 1.098c.699.402 1.637.402 3.513.402s2.813 0 3.512-.402a3 3 0 0 0 1.102-1.098c.224-.387.324-.847.368-1.5" /></g></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="2 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          color="currentColor"
+        >
+          <circle cx="5" cy="6" r="2" />
+          <path d="M21 7c-.044-.653-.144-1.113-.368-1.5a3 3 0 0 0-1.102-1.098C18.831 4 17.894 4 16.018 4s-2.814 0-3.513.402A3 3 0 0 0 11.403 5.5C11 6.196 11 7.13 11 9v6c0 1.87 0 2.804.403 3.5c.265.456.645.835 1.102 1.098c.699.402 1.637.402 3.513.402s2.813 0 3.512-.402a3 3 0 0 0 1.102-1.098c.224-.387.324-.847.368-1.5" />
+        </g>
+      </svg>
     ),
     F: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="1 0 24 24" width='1em' height='1em'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor"><circle cx="5" cy="6" r="2" /><path d="M21 4h-3c-2.828 0-4.243 0-5.121.879C12 5.757 12 7.172 12 10v10m0-8h7" /></g></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="1 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          color="currentColor"
+        >
+          <circle cx="5" cy="6" r="2" />
+          <path d="M21 4h-3c-2.828 0-4.243 0-5.121.879C12 5.757 12 7.172 12 10v10m0-8h7" />
+        </g>
+      </svg>
     ),
     settings: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor"><path d="M15.5 12a3.5 3.5 0 1 1-7 0a3.5 3.5 0 0 1 7 0" /><path d="M20.79 9.152C21.598 10.542 22 11.237 22 12s-.403 1.458-1.21 2.848l-1.923 3.316c-.803 1.384-1.205 2.076-1.865 2.456s-1.462.38-3.065.38h-3.874c-1.603 0-2.405 0-3.065-.38s-1.062-1.072-1.865-2.456L3.21 14.848C2.403 13.458 2 12.763 2 12s.403-1.458 1.21-2.848l1.923-3.316C5.936 4.452 6.338 3.76 6.998 3.38S8.46 3 10.063 3h3.874c1.603 0 2.405 0 3.065.38s1.062 1.072 1.865 2.456z" /></g></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          color="currentColor"
+        >
+          <path d="M15.5 12a3.5 3.5 0 1 1-7 0a3.5 3.5 0 0 1 7 0" />
+          <path d="M20.79 9.152C21.598 10.542 22 11.237 22 12s-.403 1.458-1.21 2.848l-1.923 3.316c-.803 1.384-1.205 2.076-1.865 2.456s-1.462.38-3.065.38h-3.874c-1.603 0-2.405 0-3.065-.38s-1.062-1.072-1.865-2.456L3.21 14.848C2.403 13.458 2 12.763 2 12s.403-1.458 1.21-2.848l1.923-3.316C5.936 4.452 6.338 3.76 6.998 3.38S8.46 3 10.063 3h3.874c1.603 0 2.405 0 3.065.38s1.062 1.072 1.865 2.456z" />
+        </g>
+      </svg>
     ),
     add: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" color="currentColor"><path d="M12 8v8m4-4H8" /><circle cx="12" cy="12" r="10" /></g></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          color="currentColor"
+        >
+          <path d="M12 8v8m4-4H8" />
+          <circle cx="12" cy="12" r="10" />
+        </g>
+      </svg>
     ),
     light: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 12a5 5 0 1 1-10 0a5 5 0 0 1 10 0M12 2v1.5m0 17V22m7.07-2.929l-1.06-1.06M5.99 5.989L4.928 4.93M22 12h-1.5m-17 0H2m17.071-7.071l-1.06 1.06M5.99 18.011l-1.06 1.06" color="currentColor" /></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M17 12a5 5 0 1 1-10 0a5 5 0 0 1 10 0M12 2v1.5m0 17V22m7.07-2.929l-1.06-1.06M5.99 5.989L4.928 4.93M22 12h-1.5m-17 0H2m17.071-7.071l-1.06 1.06M5.99 18.011l-1.06 1.06"
+          color="currentColor"
+        />
+      </svg>
     ),
     dark: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21.5 14.078A8.557 8.557 0 0 1 9.922 2.5C5.668 3.497 2.5 7.315 2.5 11.873a9.627 9.627 0 0 0 9.627 9.627c4.558 0 8.376-3.168 9.373-7.422" color="currentColor" /></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M21.5 14.078A8.557 8.557 0 0 1 9.922 2.5C5.668 3.497 2.5 7.315 2.5 11.873a9.627 9.627 0 0 0 9.627 9.627c4.558 0 8.376-3.168 9.373-7.422"
+          color="currentColor"
+        />
+      </svg>
     ),
     system: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M14 2h-4c-3.28 0-4.919 0-6.081.814a4.5 4.5 0 0 0-1.105 1.105C2 5.08 2 6.72 2 10s0 4.919.814 6.081a4.5 4.5 0 0 0 1.105 1.105C5.08 18 6.72 18 10 18h4c3.28 0 4.919 0 6.081-.814a4.5 4.5 0 0 0 1.105-1.105C22 14.92 22 13.28 22 10s0-4.919-.814-6.081a4.5 4.5 0 0 0-1.105-1.105C18.92 2 17.28 2 14 2m-3 13h2m1.5 7l-.316-.419c-.71-.944-.887-2.387-.437-3.581M9.5 22l.316-.419c.71-.944.887-2.387.437-3.581M7 22h10" color="currentColor" /></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M14 2h-4c-3.28 0-4.919 0-6.081.814a4.5 4.5 0 0 0-1.105 1.105C2 5.08 2 6.72 2 10s0 4.919.814 6.081a4.5 4.5 0 0 0 1.105 1.105C5.08 18 6.72 18 10 18h4c3.28 0 4.919 0 6.081-.814a4.5 4.5 0 0 0 1.105-1.105C22 14.92 22 13.28 22 10s0-4.919-.814-6.081a4.5 4.5 0 0 0-1.105-1.105C18.92 2 17.28 2 14 2m-3 13h2m1.5 7l-.316-.419c-.71-.944-.887-2.387-.437-3.581M9.5 22l.316-.419c.71-.944.887-2.387.437-3.581M7 22h10"
+          color="currentColor"
+        />
+      </svg>
     ),
     backward: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.808 9.441L6.774 7.47C8.19 6.048 8.744 5.284 9.51 5.554c.957.337.642 2.463.642 3.18c1.486 0 3.032-.131 4.497.144C19.487 9.787 21 13.715 21 18c-1.37-.97-2.737-2.003-4.382-2.452c-2.054-.562-4.348-.294-6.465-.294c0 .718.314 2.844-.642 3.181c-.868.306-1.321-.494-2.737-1.915l-1.966-1.972C3.603 13.338 3 12.733 3 11.995c0-.74.603-1.344 1.808-2.554" color="currentColor" /></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M4.808 9.441L6.774 7.47C8.19 6.048 8.744 5.284 9.51 5.554c.957.337.642 2.463.642 3.18c1.486 0 3.032-.131 4.497.144C19.487 9.787 21 13.715 21 18c-1.37-.97-2.737-2.003-4.382-2.452c-2.054-.562-4.348-.294-6.465-.294c0 .718.314 2.844-.642 3.181c-.868.306-1.321-.494-2.737-1.915l-1.966-1.972C3.603 13.338 3 12.733 3 11.995c0-.74.603-1.344 1.808-2.554"
+          color="currentColor"
+        />
+      </svg>
     ),
     X: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><g fill="currentColor" fillRule="evenodd" clipRule="evenodd"><path d="M5.47 5.47a.75.75 0 0 1 1.06 0l12 12a.75.75 0 1 1-1.06 1.06l-12-12a.75.75 0 0 1 0-1.06" /><path d="M18.53 5.47a.75.75 0 0 1 0 1.06l-12 12a.75.75 0 0 1-1.06-1.06l12-12a.75.75 0 0 1 1.06 0" /></g></svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
+          <path d="M5.47 5.47a.75.75 0 0 1 1.06 0l12 12a.75.75 0 1 1-1.06 1.06l-12-12a.75.75 0 0 1 0-1.06" />
+          <path d="M18.53 5.47a.75.75 0 0 1 0 1.06l-12 12a.75.75 0 0 1-1.06-1.06l12-12a.75.75 0 0 1 1.06 0" />
+        </g>
+      </svg>
     ),
     sunrise: (
-      <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9.5 6.5C9.992 5.994 11.3 4 12 4m2.5 2.5C14.008 5.994 12.7 4 12 4m0 0v6m6.363.636L16.95 12.05M3 17h2m.637-6.364L7.05 12.05M21 17h-2m2 3H3m13-3a4 4 0 0 0-8 0" color="currentColor" /></svg>
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M9.5 6.5C9.992 5.994 11.3 4 12 4m2.5 2.5C14.008 5.994 12.7 4 12 4m0 0v6m6.363.636L16.95 12.05M3 17h2m.637-6.364L7.05 12.05M21 17h-2m2 3H3m13-3a4 4 0 0 0-8 0"
+          color="currentColor"
+        />
+      </svg>
     ),
     sunset: (
-      <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='1em' height='1em'><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9.5 7.5c.492.506 1.8 2.5 2.5 2.5m2.5-2.5c-.492.506-1.8 2.5-2.5 2.5m0 0V4m6.363 6.636L16.95 12.05M3 17h2m.637-6.364L7.05 12.05M21 17h-2m2 3H3m13-3a4 4 0 0 0-8 0" color="currentColor" /></svg>
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M9.5 7.5c.492.506 1.8 2.5 2.5 2.5m2.5-2.5c-.492.506-1.8 2.5-2.5 2.5m0 0V4m6.363 6.636L16.95 12.05M3 17h2m.637-6.364L7.05 12.05M21 17h-2m2 3H3m13-3a4 4 0 0 0-8 0"
+          color="currentColor"
+        />
+      </svg>
     ),
-  }
+  };
   return Icon[name];
 }
 
